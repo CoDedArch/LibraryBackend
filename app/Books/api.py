a3d3984eaebc5cffb1749b57272faca2b2d3f23a
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2024-08-02 21:32:33 +0000

    Expose books related to a publisher

--- app/Books/api.py
+++ app/Books/api.py
@@ -1,6 +1,7 @@
 from ninja import Router
 from typing import List
-from .schemas import (ReaderSchema, GenreSchema, BookSchema, ReadingStatusSchema, BookActivitySchema, RatingSchema)
+from django.shortcuts import get_object_or_404
+from .schemas import (ReaderSchema, GenreSchema, BookSchema, ReadingStatusSchema, BookActivitySchema, RatingSchema, BookAuthorSchema)
 from .models import (Reader, Genre, Book, ReadingStatus, BookActivity, Rating)
 router = Router()
 
@@ -23,7 +24,7 @@ def list_books_by_genre(request, genre: int):
             "id": book.id,
             "title": book.title,
             "book_type": book.book_type,
-            "cover_img": book.cover_img.url if book.cover_img else None,
+            "cover_img": request.build_absolute_uri(book.cover_img.url) if book.cover_img else None,
             "publication_date": book.publication_date,
             "no_pages": book.no_pages,
             "LibraryId": book.LibraryId,
@@ -41,6 +42,43 @@ def list_books_by_genre(request, genre: int):
         }
         for book in books
     ]
+@router.get("/publisher/{publisher}", response=List[BookAuthorSchema])
+def list_books_by_genre(request, publisher: str):
+    books = Book.objects.filter(publisher=publisher)
+    return [
+        {
+            "id": book.id,
+            "title": book.title,
+            "book_type": book.book_type,
+            "cover_img": request.build_absolute_uri(book.cover_img.url) if book.cover_img else None,
+            "publication_date": book.publication_date
+        }
+        for book in books
+    ]
+
+@router.get("/{book_id}", response=BookSchema)
+def a_book(request, book_id:int):
+    book = get_object_or_404(Book, id=book_id)
+    return {
+        "id": book.id,
+        "title": book.title,
+        "book_type": book.book_type,
+        "cover_img": request.build_absolute_uri(book.cover_img.url) if book.cover_img else None,
+        "publication_date": book.publication_date,
+        "no_pages": book.no_pages,
+        "LibraryId": book.LibraryId,
+        "isbn10": book.isbn10,
+        "isbn13": book.isbn13,
+        "publisher": book.publisher,
+        "genre_id": book.genre.id if book.genre else None,
+        "reader_id": book.reader.id if book.reader else None,
+        "average_rating": book.average_rating(),
+        "number_of_ratings": book.number_of_ratings(),
+        "readers_currently_reading": book.readers_currently_reading(),
+        "readers_finished_reading": book.readers_finished_reading(),
+        "total_downloads": book.total_downloads(),
+        "total_shares": book.total_shares()
+    }
 
 @router.get("", response=List[BookSchema])
 def list_books(request):
