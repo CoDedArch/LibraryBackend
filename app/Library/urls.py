d1411663cca9a01789771d1398e95b795456a031
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2024-08-02 21:36:22 +0000

    uploaded new books

--- app/Library/urls.py
+++ app/Library/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from .api import api
+from django.conf import settings
 #BookDownload download books to the cloud
 #bookmarking
 #search
