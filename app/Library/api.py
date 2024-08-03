7615502313f0d61c379a05dea1d83bb2fb3dabf4
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2024-08-03 16:35:20 +0000

    Add: new router instance

--- app/Library/api.py
+++ app/Library/api.py
@@ -1,22 +1,13 @@
+from django.contrib.auth import authenticate
 from ninja import NinjaAPI, Schema
 from ninja_jwt.controller import NinjaJWTDefaultController
+from ninja_jwt.tokens import RefreshToken
 from ninja_jwt.authentication import JWTAuth
 from ninja_extra import NinjaExtraAPI
-
+from ninja.errors import HttpError
 
 api = NinjaExtraAPI()
 api.register_controllers(NinjaJWTDefaultController)
 api.add_router("/books", "Books.api.router")
+api.add_router("/user/", "Books.user_api.router")
 # api.add_router("/genres", "Books.api.router")
-
-class UserSchema(Schema):
-    username: str
-    is_authenticated: bool
-
-@api.get('/hello')
-def hello(request):
-    return "Hello world"
-
-@api.get('/me', response=UserSchema, auth=JWTAuth())
-def me(request):
-    return request.user
