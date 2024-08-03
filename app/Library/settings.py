21b2b2da4517ea4f4e6975fe89d54bd30fa8722a
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2024-08-03 16:35:48 +0000

    modify settings

--- app/Library/settings.py
+++ app/Library/settings.py
@@ -141,5 +141,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 NiNJA_JWT = {
     'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=7)
+    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=7),
 }
