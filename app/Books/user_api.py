d506e49b8079a6bf42751c422d1a0d039eb3ce83
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2024-08-04 18:17:21 +0000

    done

--- app/Books/user_api.py
+++ app/Books/user_api.py
@@ -9,4 +9,4 @@ router = Router()
 def create_user(request, payload:SignupSchema):
     user = User.objects.create_user(username=payload.username, password=payload.password, email=payload.email)
     reader = Reader.objects.create(user=user, fullname=payload.fullname, date_of_birth=payload.date_of_birth, city=payload.city)
-    return {"success": True, "user_id": user.id}
+    return {"ok": True, "user_id": user.id}
